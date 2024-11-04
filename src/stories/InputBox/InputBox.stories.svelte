16d3bb845f86a401a44ba5eaca623f263987f472
unknown <kjh36102@gmail.com>
2024-11-04 19:40:00 +0900

    storybook 사용 준비 및 테스트 완료

---
+++ src/stories/InputBox/InputBox.stories.svelte
@@ -0,0 +1,12 @@
+<script module lang="ts">
+    import InputBox from "./InputBox.svelte";
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+
+
+    const { Story } = defineMeta({
+        title: 'InputBox',
+        component: InputBox
+    })
+</script>
+
+<Story name="InputBoxTest" args={{ textContent: "기본" }}></Story>
---
+++ src/stories/InputBox/InputBox.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+    export let textContent: string;
+
+</script>
+
+<input bind:value={textContent}>
