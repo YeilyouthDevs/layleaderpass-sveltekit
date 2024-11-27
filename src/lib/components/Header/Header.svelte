87754d99439ca4d5c1db76f4e7aba1a33da04938
unknown <kjh36102@gmail.com>
2024-11-27 09:27:41 +0900

    최초 커밋

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	
+</script>
+
+<div class="border-b border-zinc-300 p-2">
+	<div class="flex items-center gap-2">
+        <img class="h-7" src="logo.png" alt="로고">
+        <p class="font-bold text-lg text-zinc-900">중직자PASS</p>
+    </div>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Header from '$lib/components/Header/Header.svelte';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<Header />
+{@render children()}
